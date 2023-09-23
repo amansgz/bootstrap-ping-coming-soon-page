ddaf74b5f5c712e06da08defee29e321c0873046
amansgz <amansgz.ar@gmail.com>
2023-09-23 11:41:04 -0300

    Update layout and styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,37 @@
-const formInput = document.getElementById('formInput');
-const btnSubmit = document.getElementById('button-addon2');
+const emailInput = document.getElementById('email');
+const btnSubmit = document.getElementById('btn-submit');
 
 const errorMessage = document.querySelector('.message-error');
 
 btnSubmit.addEventListener('click', e => {
   e.preventDefault();
 
-  const emailValue = formInput.value;
+  const emailValue = emailInput.value;
   const validation = validate_email(emailValue);
 
   
   if(emailValue == '') {
-    formInput.classList.add('input-email-error');
+    emailInput.classList.add('input-email-error');
     errorMessage.style.display = 'block';
     errorMessage.textContent = 'Whoops! It looks like you forgot to add your email';
   }
 
   if (!validation) {
-    formInput.classList.add('input-email-error');
-    errorMessage.style.display = 'block';
+    emailInput.classList.add('input-email-error');
+    errorMessage.style.opacity = '1';
     errorMessage.textContent = 'Please provide a valid email address';
     btnSubmit.style.backgroundColor = 'hsl(223, 87%, 75%)';
   }
   if(validation) {
-    formInput.value = '';
-    formInput.classList.remove('input-email-error');
-    errorMessage.style.display = 'none';
+    emailInput.value = '';
+    emailInput.classList.remove('input-email-error');
+    errorMessage.style.opacity = '0';
     btnSubmit.style.backgroundColor = 'hsl(223, 87%, 63%)';
   }
 })
 
-function validate_email(email) {/* 
-    const regex =  /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/; */
+function validate_email(email) {
+    const regex =  /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
     
     if (email.match(regex)) {
       return true;
